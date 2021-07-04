573aab5fdfb314013c3444dfcc0c8f423736a701
Ines-jammeli <jammeli.ines@gmail.com>
2021-07-04 23:10:27 +0200

    middle

--- animalsList.js
+++ animalsList.js
@@ -12,28 +12,7 @@ var catsList=[
 {name: "jarjoum", breed: "Tunisian cat",src: "https://farm5.static.flickr.com/4856/46828271741_f8fd486114.jpg", id: "jarjoum_img", alt: "jarjoum", age:1  },
 {name: "falfoula", breed: "Tunisian cat",src: "https://st3.depositphotos.com/35165130/37747/i/450/depositphotos_377474434-stock-photo-cats-tunisia-sousse-medina-tunisia.jpg", id: "falfoula_img", alt: "falfoula", age:2  }
 ]
-/**
-function existingDogs(){
-$("div#change").empty();
 
-    for (i=0;i<dogsList.length;i++){
-	    $("div#change").append("<a href=\"app_form.html\"> <div class=\"gallery\"> <img src=\""+dogsList[i].src +" id=\""+ dogsList[i].id+ "alt=\""+dogsList[i].name+ "width=\"300\" height=\"200\">")
-	    $("div#change").append("<h3 name=\""+ dogsList[i].name +"\">"+ dogsList[i].name +"</h3>")
-  		$("div#change").append("<p>"+ dogsList[i].age+ "year(s) old </p> </div> </a>")
-    }
-}
-
-
-function existingCats(){
-$("div#change").empty(); 
-
-    for (i=0;i<catsList.length;i++){
-	    $("div#change").append("<a href=\"app_form.html\"> <div class=\"gallery\"> <img src=\""+catsList[i].src +"id=\""+ catsList[i].id+ "alt=\""+catsList[i].name+ "width=\"300\" height=\"200\">")
-	    $("div#change").append("<h3 name=\""+ catsList[i].name +"\">"+ catsList[i].name +"</h3>")
-  		$("div#change").append("<p>"+ catsList[i].age+ "year(s) old </p> </div> </a>")
-    }
-}
-**/
 
 var contactList=[{Name: 'Houda Bergaoui', email: 'houda@gmail.com', tel:'52 194 155'},
 {Name: 'Souhayla Rhouma', email: 'souhayla@gmail.com', tel:'98 879 155'},
@@ -45,16 +24,23 @@ var contactList=[{Name: 'Houda Bergaoui', email: 'houda@gmail.com', tel:'52 194 
 ]
 
 
+
+dogsList= JSON.parse(localStorage.getItem("dogsList"));
+catsList= JSON.parse(localStorage.getItem("catsList"));
+contactList= JSON.parse(localStorage.getItem("contactList"));
+
 if (localStorage.page==="file:///C:/Users/Ines/Desktop/git/Association_website/dog.html"){
    for (var i=0;i<dogsList.length;i++){
     if (localStorage.src===dogsList[i].src){
       dogsList.splice(i,1);
+      localStorage.setItem("dogsList", JSON.stringify(dogsList));
     }
    }}
    else if (localStorage.page==="file:///C:/Users/Ines/Desktop/git/Association_website/cat.html"){
     for (var i=0;i<catsList.length;i++){
     if (localStorage.src===catsList[i].src){
       catsList.splice(i,1);
+      localStorage.setItem("catsList", JSON.stringify(catsList));
     }
    }
   
